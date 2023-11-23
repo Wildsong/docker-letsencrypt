7caa85f18ac5e8a793dd01d1c8a4c379dd58bf5c
Brian Wilson <brian@wildsong.biz>
2023-11-23 14:38:24 -0800

    moving certs to docker vol

--- build_hitch_bundle.sh
+++ build_hitch_bundle.sh
@@ -6,8 +6,7 @@
 #  
 docker run --rm --env-file=.env -v certs:/etc/letsencrypt debian /usr/bin/bash -c '
   cd /etc/letsencrypt/live/$CERTNAME
-  cat privkey.pem fullchain.pem ../../dhparams.pem > /etc/letsencrypt/hitch-bundle.pem
+  cat privkey.pem fullchain.pem ../../dhparams.pem > /etc/letsencrypt/hitch-bundle.pem 
   ls -l /etc/letsencrypt/hitch-bundle.pem
 '
-
 
--- run_cloudflare_certbot.sh
+++ run_cloudflare_certbot.sh
@@ -2,14 +2,16 @@
 # This gets run from crontab to keep certificates up to date.
 # Read options here https://eff-certbot.readthedocs.io/en/stable/using.html#configuration-file
 
-# --cert-name will add or remove names using the certificate as named
-# --expand will only add names to an existing cert
-
-# "certonly" obtains certs without installing them (except in /etc/letsencrypt of course)
-# Add this in case some domains aren't working: --allow-subset-of-names
-
 source .env
-docker run --rm -v certs:/etc/letsencrypt:rw cc/certbot \
+
+function certbot () {
+
+    # "certonly" obtains certs without installing them (except in /etc/letsencrypt of course)
+    # Add this in case some domains aren't working: --allow-subset-of-names
+    # --cert-name will add or remove names using the certificate as named
+    # --expand will only add names to an existing cert
+
+    docker run --rm -v certs:/etc/letsencrypt:rw cc/certbot \
        certonly \
        --cert-name ${CERTNAME} \
        --expand \
@@ -22,17 +24,22 @@ docker run --rm -v certs:/etc/letsencrypt:rw cc/certbot \
        --dns-cloudflare --dns-cloudflare-credentials /usr/local/lib/cloudflare.ini \
        --dns-cloudflare-propagation-seconds=30 \
        --noninteractive
-#--quiet
+    # --dry-run
+    # --quiet
+}
 
+function update_hitch () {
+    age=`docker run --rm -v certs:/etc/letsencrypt:rw debian stat -c '%Y' /etc/letsencrypt/hitch-bundle.pem`
+    now=$(date +"%s")
+    if (( ($now - $age) < (60 * 60) )); then
+	echo $hitch changed
+	#docker stack rm varnish
+	docker compose -f ~/docker/varnish/compose.yaml down
+	sleep 5
+	# docker stack deploy --with-registry-auth -c $HOME/docker/varnish/compose.yaml varnish 
+	docker compose -f ~/docker/varnish/compose.yaml up -d
+    fi
+}
 
-# Update hitch
-hitch="certs/hitch-bundle.pem"
-age=$(stat -c %Y $hitch)
-now=$(date +"%s")
-if (( ($now - $age) < (60 * 60) )); then
-    echo $hitch changed
-    docker stack rm varnish
-    # give varnish_network time to disappear
-    sleep 5
-    docker stack deploy --with-registry-auth -c $HOME/docker/varnish/compose.yaml varnish 
-fi
+certbot
+update_hitch
