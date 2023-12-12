a8d0d4568465dd5c128804e47d33f1db4a46ebfa
bwilsoncc <bwilson@clatsopcounty.gov>
2023-12-12 08:49:37 -0800

    fixed, actually works now!

--- build_hitch_bundle.sh
+++ build_hitch_bundle.sh
@@ -4,7 +4,7 @@
 #  If there are files missing try running the run_*.sh script for your setup.
 #  It relies on CERTNAME from your .env file.
 #  
-docker run --rm --env-file=.env -v certs:/etc/letsencrypt debian /usr/bin/bash -c '
+docker run --rm --env-file=.env -v certs:/etc/letsencrypt debian /bin/sh -c '
   cd /etc/letsencrypt/live/$CERTNAME
   cat privkey.pem fullchain.pem ../../dhparams.pem > /etc/letsencrypt/hitch-bundle.pem 
   ls -l /etc/letsencrypt/hitch-bundle.pem
